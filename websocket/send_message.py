d621837cbc5cf999fa710646bc8434571d478244
nickdurante <ng.durante@gmail.com>
2020-03-25 15:03:54 +0100

    Send message (client) should be ready

--- websocket/send_message.py
+++ websocket/send_message.py
@@ -4,11 +4,32 @@ import websockets
 import json
 from sys import argv
 
+
+"""
+    Takes argv[1] and sends it to the server sepcified
+    python3 send_message.py '{"type": "message", "content": "rocknroll"}'
+"""
 IP = "192.168.4.1"
 PORT = "6789"
-async def hello():
-    uri = "ws://"+ IP + ":" + PORT
-    async with websockets.connect(uri) as websocket:
-        await websocket.send(json.dumps({"type": "message", "from": "RPi3", "content": argv[1]}))
+uri = "ws://"+ IP + ":" + PORT
 
-asyncio.get_event_loop().run_until_complete(hello())
+async def send_message():
+
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(argv[1])
+
+
+def main():
+    if len(argv) != 2:
+        print("Invalid number of inputs")
+        return
+    try:
+        json.loads(argv[1])
+    except:
+        print("Invalid JSON input")
+        return
+
+    asyncio.get_event_loop().run_until_complete(send_message())
+
+if __name__ == '__main__':
+    main()
