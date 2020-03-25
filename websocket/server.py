97c87c2972e0a51e5dee19786bf04e877e61f425
nickdurante <ng.durante@gmail.com>
2020-03-25 15:38:47 +0100

    Cleanup old files

--- websocket/server.py
+++ websocket/server.py
@@ -1,73 +1,25 @@
-#!/usr/bin/env python
-
-# WS server example that synchronizes state across clients
-
-import asyncio
-import json
-import websockets
 import serial
+import asyncio
+import websockets
+import json
 
-IP = "0.0.0.0"
-PORT = 6789
+IP = "192.168.4.1"
+PORT = "6789"
+ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
 
-
-STATE = {"value": 0}
-USERS = set()
-MESSAGE_LIST = []
-
-def state_event():
-    return json.dumps({"type": "state", **STATE})
-
-
-def users_event():
-    return json.dumps({"type": "users", "count": len(USERS)})
-
-def serial_event():
-    last_message = MESSAGE_LIST[len(MESSAGE_LIST) - 1]
-
-    return json.dumps(last_message)
+def main():
+    while True:
+        while ser.in_waiting:
+            message_in = ser.readline().decode()
+            print("Read: " + message_in )
+            asyncio.get_event_loop().run_until_complete(hello("Arduino", message_in))
 
 
-async def notify_state():
-    if USERS:  # asyncio.wait doesn't accept an empty list
-        message = state_event()
-        await asyncio.wait([user.send(message) for user in USERS])
+async def hello(sender, message):
+    uri = "ws://"+ IP + ":" + PORT
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(json.dumps({"type": "message", "from": sender, "content": message}))
 
 
-async def notify_users():
-    if USERS:  # asyncio.wait doesn't accept an empty list
-        message = users_event()
-        await asyncio.wait([user.send(message) for user in USERS])
-
-async def broadcast_message(message):
-    await asyncio.wait([user.send(message) for user in USERS])
-
-
-async def register(websocket):
-    USERS.add(websocket)
-    await notify_users()
-
-
-async def unregister(websocket):
-    USERS.remove(websocket)
-    await notify_users()
-
-
-async def counter(websocket, path):
-    # register(websocket) sends user_event() to websocket
-    print("Running server at: " + IP + " " + str(PORT))
-    await register(websocket)
-    try:
-        await websocket.send(state_event())
-        async for message in websocket:
-            data = json.loads(message)
-            print(data)
-            await broadcast_message(message)
-    finally:
-        await unregister(websocket)
-
-
-start_server = websockets.serve(counter, IP, PORT)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+if __name__ == '__main__':
+    main()
